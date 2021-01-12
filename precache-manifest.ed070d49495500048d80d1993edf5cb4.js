77287339aaaff4c36d832f6da54afe39e6a2b47f
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-01-12 20:59:11 +0530

    Updates

---
+++ precache-manifest.ed070d49495500048d80d1993edf5cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aaf2683dd85443ff1b5f19a4914a3ae",
+    "url": "/ai-news-app/index.html"
+  },
+  {
+    "revision": "83943de698e15efe1ee2",
+    "url": "/ai-news-app/static/css/main.e4daf68c.chunk.css"
+  },
+  {
+    "revision": "3ac7d1da894a554653c3",
+    "url": "/ai-news-app/static/js/2.e7c5ecda.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ai-news-app/static/js/2.e7c5ecda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83943de698e15efe1ee2",
+    "url": "/ai-news-app/static/js/main.a2c78d0d.chunk.js"
+  },
+  {
+    "revision": "080b182f4814543c91c9",
+    "url": "/ai-news-app/static/js/runtime-main.cf33ddff.js"
+  }
+]);
